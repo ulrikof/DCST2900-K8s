671aaaa1dd066d1e99135baa7d909e5a06938fc4
ulrikof <ulrikof@stud.ntnu.no>
2025-03-05 12:25:23 +0100

    created install file for kubevirt

---
+++ KubeVirt/kubevirt-install.sh
@@ -0,0 +1,29 @@
+# Followed this guide: https://kubevirt.io/quickstart_cloud/
+
+### 1. Install and deploy KubeVirt
+
+# Get newest version
+export VERSION=$(curl -s https://storage.googleapis.com/kubevirt-prow/release/kubevirt/kubevirt/stable.txt)
+echo $VERSION
+
+# Deploy KubeVirt operator
+kubectl create -f "https://github.com/kubevirt/kubevirt/releases/download/${VERSION}/kubevirt-operator.yaml"
+
+# Deploy KubeVirt custom resource definitions
+kubectl create -f "https://github.com/kubevirt/kubevirt/releases/download/${VERSION}/kubevirt-cr.yaml"
+
+# Check deployment
+kubectl get kubevirt.kubevirt.io/kubevirt -n kubevirt -o=jsonpath="{.status.phase}"
+
+# Check components
+kubectl get all -n kubevirt
+
+### 2. Install virtctl for "quick access to the serial and graphical ports of a VM and also handle start/stop operations."
+
+VERSION=$(kubectl get kubevirt.kubevirt.io/kubevirt -n kubevirt -o=jsonpath="{.status.observedKubeVirtVersion}")
+ARCH=$(uname -s | tr A-Z a-z)-$(uname -m | sed 's/x86_64/amd64/') || windows-amd64.exe
+echo ${ARCH}
+curl -L -o virtctl https://github.com/kubevirt/kubevirt/releases/download/${VERSION}/virtctl-${VERSION}-${ARCH}
+chmod +x virtctl
+sudo install virtctl /usr/local/bin
+
