58c8e1b73b6fc6e33e58566dd64167582f6379fc
ulrikof <ulrikof@stud.ntnu.no>
2025-03-06 18:00:21 +0100

    patch machien

---
+++ storage/hm.sh
@@ -0,0 +1,4 @@
+talosctl patch machineconfig -n 192.168.0.119,192.168.0.156,192.168.0.77 --patch @mayastor-patch.yaml
+
+talosctl patch machineconfig -n 192.168.0.82,192.168.0.38,192.168.0.55 --patch @mayastor-patch-cp.yaml
+
