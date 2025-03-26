bf1c697776260d484ef9024278b9a4e06251c0e2
ulrikof <ulrikof@stud.ntnu.no>
2025-03-26 16:32:57 +0100

    added files for talos in kubevirt

--- KubeVirt/talos-vm/talos-config.sh
+++ KubeVirt/talos-vm/talos-config.sh
@@ -2,7 +2,7 @@
 export IP=10.244.8.74
 
 # 1. Generate config
-talosctl gen config K8s https://$IP:6443 --config-patch @patch.yaml
+talosctl gen config K8s https://$IP:6443 --config-patch @all-patch.yaml --config-patch-worker @worker-patch.yaml
 
 # 2. Set endpoint and node
 talosctl --talosconfig talosconfig config endpoint $IP
@@ -13,3 +13,18 @@ export TALOSCONFIG=~/talos/talosconfig
 
 # 4. Apply config
 talosctl apply-config --insecure --nodes $IP --file controlplane.yaml
+
+# 5. Bootstrap the cluster
+talosctl bootstrap
+
+# 6. Retrive Kubeconfig
+talosctl kubeconfig .
+
+# 7. Export kubeconfig so that kubectl can get access to the cluster (should be added to bashrc)
+
+export KUBECONFIG=~/talos/kubeconfig
+
+# 8. Verify cluster
+
+# get nodes:
+kubectl get nodes
