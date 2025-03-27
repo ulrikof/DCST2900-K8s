7b0b5ab276abc11644b693596ffb2890a961bd97
Jonas Haugland <jonashaugland@hotmail.com>
2025-03-27 09:33:06 +0100

    Finishing up "getting started"

--- ArgoCD/argocd.sh
+++ ArgoCD/argocd.sh
@@ -5,17 +5,20 @@
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
+
 ## 2. Download ArgoCD CLI
 
 curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
 sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
 rm argocd-linux-amd64
 
+
 ## 3. Access The ArgoCD API Server
 
 ### Port Forwarding
 
 kubectl port-forward svc/argocd-server -n argocd 8080:443
+
 
 ## 4. Login using the CLI
 
@@ -32,12 +35,55 @@ argocd login localhost:8080
 
 argocd account update-password
 
+
 ## 5. Register A Cluster To Deploy Apps To (Optional)
+
 
 ## 6. Create An Application From A Git Repository
 
 ### Creating Apps via CLI
 
-kubectl config set-context --current --namespace=argocd
+kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "NodePort"}}'
 #example guestbook app
 argocd app create guestbook --repo https://github.com/argoproj/argocd-example-apps.git --path guestbook --dest-server https://kubernetes.default.svc --dest-namespace default
+
+
+## 7. Sync (Deploy) The Application
+
+### View the application status
+#argocd app get <APP_NAME>
+argocd app get guestbook
+
+### Sync (Deploy) the application
+#argocd app sync <APP_NAME>
+argocd app sync guestbook
+
+
+## 8. Make changes to the application
+
+### Find the Argo CD Server Endpoint
+kubectl get svc argocd-server -n argocd
+
+### Change namespace
+#argocd app set guestbook --dest-namespace <NEW_NAMESPACE>
+argocd app set guestbook --dest-namespace guestbook
+
+### Sync application manually
+#argocd app sync <APP_NAME>
+argocd app sync guestbook
+
+### Sync application automatically
+#argocd app set <APP_NAME> --sync-policy <POLICY>
+argocd app set guestbook --sync-policy automated
+
+### Refresh app status
+#argocd app get <APP_NAME> --refresh
+argocd app get guestbook --refresh
+
+### Enable self-healing
+#argocd app set <APP_NAME> --auto-prune --self-heal
+argocd app set guestbook --auto-prune --self-heal
+
+### Delete the application
+#argocd app delete <APP_NAME>
+argocd app delete guestbook
