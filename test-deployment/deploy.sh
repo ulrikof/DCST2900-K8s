4655f41914ae511693add26fdef2771b16021c33
ulrikof <ulrikof@stud.ntnu.no>
2025-04-01 15:00:07 +0200

    addded files for automatically exposing exposed child cluster services in parent cluster

--- test-deployment/deploy.sh
+++ test-deployment/deploy.sh
@@ -2,6 +2,9 @@
 kubectl apply -f configmap.yaml
 kubectl apply -f deployment.yaml
 
-# Expose it
+# Expose it with random external port
 kubectl expose deployment test-nginx --type=NodePort --port=80 --name=test-nginx
+
+# or with bound port 
+kubectl apply -f expose.yaml
 
