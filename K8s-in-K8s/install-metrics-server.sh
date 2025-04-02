b1a7642c0a8a6f273d39ef1937c897e733876e8e
ulrikof <ulrikof@stud.ntnu.no>
2025-04-02 10:01:34 +0200

    added hpa files

---
+++ K8s-in-K8s/install-metrics-server.sh
@@ -0,0 +1,4 @@
+kubectl apply -f https://raw.githubusercontent.com/alex1989hu/kubelet-serving-cert-approver/main/deploy/standalone-install.yaml
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+
+# or can be installed with extra manifests
