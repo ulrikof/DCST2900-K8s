dc5bccb0211b475333c41084fdc21051b556ab64
yeezus9000 <github@estlan.net>
2025-04-07 14:59:39 +0200

    fixed install script

--- argo-cd-setup.sh
+++ argo-cd-setup.sh
@@ -2,45 +2,32 @@ kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 
-## 2. Download ArgoCD CLI
+## 2. Download ArgoCD CLI (if manager doesnt have it)
 
 curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
 sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
 rm argocd-linux-amd64
 
+# expose argocd instance and connect to repo
+(kubectl port-forward svc/argocd-server -n argocd 8080:443 &> /dev/null) &
+# If you leave the CLI without setting up permanent port-forwarding you will need to repeat this ^
+pw=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
+argocd login localhost:8080 --username admin --password $pw --insecure
 argocd repo add git@github.com:ulrikof/DCST2900-K8s.git --ssh-private-key-path ~/.ssh/git-argo-key
 
-# apiVersion: argoproj.io/v1alpha1
-# kind: Application
-# metadata:
-#   name: app-of-apps
-#   namespace: argocd
-# spec:
-#   project: default
-#   source:
-#     repoURL: git@github.com:ulrikof/DCST2900-K8s.git
-#     targetRevision: main
-#     path: ArgoCD/apps
-#     directory:
-#       recurse: true
-#   destination:
-#     server: https://kubernetes.default.svc
-#     namespace: argocd
-#   syncPolicy:
-#     automated:
-#       prune: true
-#       selfHeal: true
-#     syncOptions:
-#       - Cascade=true
-#       - PrunePropagationPolicy=foreground
-#       - PruneLast=true
+# start app-of-apps
+pwd=$(pwd)
+kubectl apply -f $pwd/app-of-apps.yaml
 
+until [[ $(kubectl get -n metallb-system ipaddresspools.metallb.io 2>/dev/null | wc -l) -gt 1 ]]; do
+    sleep 2
+    echo "Waiting for IP address pools..."
+done
 
-# argocd cli
+# patch loadbalancer into config for argo cli
 kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+ip=$(kubectl get service argocd-server -n argocd --output=jsonpath='{.status.loadBalancer.ingress[0].ip}')
 
-kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
+argocd login $ip --username admin --password $pw --insecure
 
-kubectl get service argocd-server -n argocd --output=jsonpath='{.status.loadBalancer.ingress[0].ip}'
-
-argocd login $(kubectl get service argocd-server -n argocd --output=jsonpath='{.status.loadBalancer.ingress[0].ip}') --username admin --password $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo) --insecure
+# we should probably change password at some point
