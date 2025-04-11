e3e4e8449eb0593409cfd490d59a9614de639ac6
yeezus9000 <github@estlan.net>
2025-04-11 09:44:42 +0200

    i made some changes

--- z_archive/K8s-in-K8s/patch-image.sh
+++ z_archive/K8s-in-K8s/patch-image.sh
@@ -26,12 +26,16 @@ while [[ $# -gt 0 ]]; do
 done
 
 if [[ -z "$TALOS_IP" ]]; then
-  echo "Usage: ./patch-image.sh --ip <talos-config-ip> [--worker-file <path>] [--controlplane-file <path>]"
+  echo "NOT IP DNS RESOLUTION TALK TO ULRIK Usage: ./patch-image.sh --ip <talos-config-ip> [--worker-file <path>] [--controlplane-file <path>]"
   exit 1
 fi
 
 # Construct the image URLs
-BASE_URL="https://factory.talos.dev/?arch=amd64&cmdline=talos.config%3Dhttp%3A%2F%2F${TALOS_IP}%2F"
+asdf=".$TALOS_IP.svc.cluster.local"
+BASE_URL="https://factory.talos.dev/?arch=amd64&cmdline=talos.config%3Dhttp%3A%2F%2F${asdf}%2F"
+
+
+# BASE_URL="https://factory.talos.dev/?arch=amd64&cmdline=talos.config%3Dhttp%3A%2F%2F${TALOS_IP}%2F"
 ARGS="&cmdline-set=true&extensions=-&extensions=siderolabs%2Fiscsi-tools&extensions=siderolabs%2Futil-linux-tools&platform=metal&target=metal&version=1.9.5"
 
 CONTROLPLANE_IMAGE=$(curl -s "${BASE_URL}controlplane.yaml${ARGS}" | grep -o 'https://[^"]*\.qcow2' | head -n1)
