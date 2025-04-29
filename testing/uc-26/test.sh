fa76a3343083471b7d657d689f10e839081b0277
ulrikof <ulrikof@stud.ntnu.no>
2025-04-29 16:03:59 +0200

    added more tests

---
+++ testing/uc-26/test.sh
@@ -0,0 +1,25 @@
+# This test is done in a physical cluster
+
+# Apply deployment
+kubectl apply -f http-deployment.yaml
+
+# Verify physical cluster
+kubectl config current-context
+
+# Verify deployment, pods and svc before start of test
+kubectl get deployment
+
+kubectl get pod
+
+kubectl get svc
+
+kubectl logs http-logger-<pod-id>
+kubectl logs http-logger-<pod-id>
+
+# Curl the svc four times
+for i in {1..4}; do curl -o /dev/null <external-svc-ip>; done
+
+# Check the logs, should be equall amount of requests in each.
+
+kubectl logs http-logger-<pod-id>
+kubectl logs http-logger-<pod-id>
